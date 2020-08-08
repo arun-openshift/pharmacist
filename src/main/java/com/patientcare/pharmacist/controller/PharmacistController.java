5889e8fd1754118e01ce0f2a5d3313621a07aa61
Kulanthaivel <539617@cognizant.com>
2020-08-08 10:24:17 +0530

    -minitial commit

---
+++ src/main/java/com/patientcare/pharmacist/controller/PharmacistController.java
@@ -0,0 +1,44 @@
+package com.patientcare.pharmacist.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+
+import com.patientcare.pharmacist.entity.Pillbox;
+
+
+@Controller
+public class PharmacistController {
+	
+	@Value("${PILLBOX_API}")
+	private String PILLBOX_API;
+	
+	private String ADD_PILLBOX_PATH = "/addPillbox";
+	private String GET_PILLBOX_PATH = "/getAllPillboxes";
+	
+	@RequestMapping(value = { "/getAllPillboxes" }, method = RequestMethod.GET)
+	public ResponseEntity<String> getAllPillboxes() {
+		String url = PILLBOX_API + GET_PILLBOX_PATH;
+		
+		RestTemplate restTemplate = new RestTemplate();
+		String resp = restTemplate.getForObject(url, String.class);
+		return new ResponseEntity<String>(resp, HttpStatus.OK);
+				
+	}
+	
+	@RequestMapping(value = { "/addPillbox" }, method = RequestMethod.POST)
+	public ResponseEntity<String> addPillbox(@RequestBody Pillbox pillbox) {
+		String url = PILLBOX_API + ADD_PILLBOX_PATH;
+		
+		RestTemplate restTemplate = new RestTemplate();
+		String resp = restTemplate.postForObject(url, pillbox, String.class);
+		return new ResponseEntity<String>(resp, HttpStatus.OK);
+		
+	}
+
+}
---
+++ src/main/java/com/patientcare/pharmacist/entity/Pillbox.java
@@ -0,0 +1,71 @@
+package com.patientcare.pharmacist.entity;
+
+public class Pillbox {
+
+	private Integer pillboxid;
+	private Integer userid;
+	private String status;
+	private Integer consumed;
+	private Integer remaining;
+	private String frequency;
+	private String time;
+
+	public Integer getPillboxid() {
+		return pillboxid;
+	}
+
+	public void setPillboxid(Integer pillboxid) {
+		this.pillboxid = pillboxid;
+	}
+
+	public Integer getUserid() {
+		return userid;
+	}
+
+	public void setUserid(Integer userid) {
+		this.userid = userid;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Integer getConsumed() {
+		return consumed;
+	}
+
+	public void setConsumed(Integer consumed) {
+		this.consumed = consumed;
+	}
+
+	public Integer getRemaining() {
+		return remaining;
+	}
+
+	public void setRemaining(Integer remaining) {
+		this.remaining = remaining;
+	}
+
+	public String getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(String frequency) {
+		this.frequency = frequency;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+}
+
+
